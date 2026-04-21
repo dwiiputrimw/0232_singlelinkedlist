935fb6f19300134f927d49a6b65e92e049c2a238
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-21 11:26:03 +0700

    Membuat struktur class Node dan LinkedList

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -1,3 +1,17 @@
 #include <iostream> 
 #include <string>
 using namespace std; 
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+};
+
+class LinkedList
+{
+    Node *START;
+
+public: 
+};
--- SingleLinkedListTest.cpp
+++ SingleLinkedListTest.cpp
@@ -1,41 +1,41 @@
-#include <iostream> 
+#include <iostream>
 #include <string>
-using namespace std; 
+using namespace std;
 
 class Node
 {
 public:
     int noMhs;
     Node *next;
-}; 
+};
 
 class LinkedList
 {
     Node *START;
 
-public: 
+public:
     LinkedList()
     {
         START = NULL;
     }
 
-    void addNote()
+    void addNode()
     {
         int nim;
-        cout << "\nMasukan Nomor Mahasiswa :";
+        cout << "\nMasukkan Nomor Mahasiswa: ";
         cin >> nim;
 
-        Node *nodeBaru = new Node(); 
-        nodeBaru ->noMhs = nim;
-        nodeBaru ->next = NULL;
+        Node *nodeBaru = new Node();
+        nodeBaru->noMhs = nim;
+        nodeBaru->next = NULL;
 
-        if (START == NULL || nim <= START ->noMhs)
+        if (START == NULL || nim <= START->noMhs)
         {
             if (START != NULL && nim == START -> noMhs)
             {
-                cout << "n\Duplikasi noMhs tidak diijinkan\n";
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
             }
-
 
             nodeBaru->next = START;
             START = nodeBaru;
@@ -45,22 +45,48 @@ public:
 
         Node *previous = START;
         Node *current = START;
-
-        while (current != NULL && nim > current-> noMhs)
+        
+        while (current != NULL && nim > current->noMhs)
         {
-        if (nim == current->noMhs)
-        {
-            cout << "\nDuplikasi noMhs tidak diijinkan\n";
-            return;
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
         }
-        previous = current;
-        current = nodeBaru;
 
-        }
-
-        nodeBaru ->next = current;
-        previous ->next = nodeBaru;
-
-
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+       
     }
+
+    bool listEmpty()
+    {
+        return START == NULL;
+    }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START; 
+
+        while (current != NULL && nim != current ->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search(nim, previous, current->next
+    }
+
+
 };
