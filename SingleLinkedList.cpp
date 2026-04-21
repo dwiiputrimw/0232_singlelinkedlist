9097771dee5cba60c95970a2eece4f52bed5224f
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-21 11:45:14 +0700

    Menambahkan traansversal pada fungsi search

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -70,5 +70,13 @@ public:
     {
         previous = START;
         current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
     }
 };
