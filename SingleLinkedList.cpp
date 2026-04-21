419c57f7ce9f781052353e2060def801f81ada94
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-21 11:48:02 +0700

    Menambahkan penghapusan Node

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -86,5 +86,13 @@ public:
 
         if (!search(nim, previous, current))
             return false;
+        
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true; 
     }
 };
